00255dd1e0b052d36d7ff192186a9e4d68eb27ef
Carson Fox <carson.fox@atomicjolt.com>
2020-12-07 14:58:49 -0500

    Centroid assignment

--- serial.cpp
+++ serial.cpp
@@ -4,24 +4,50 @@
 #include "common.hpp"
 
 std::vector<DataPoint> kmeans(std::vector<DataPoint> data, int k);
-void assignCentroids(std::vector<DataPoint> &data, const std::vector<DataPoint> &centroids);
+bool assignCentroids(std::vector<DataPoint> &data, const std::vector<DataPoint> &centroids);
+std::vector<DataPoint> newCentroids(const std::vector<DataPoint> &data, int k);
 
 int main(int argc, char **argv) {
     auto data = readCSV(argc, argv);
-    kmeans({}, 10);
+    data = kmeans(data, 10);
+
+    for (auto &&datum : data) {
+        std::cout << datum << std::endl;
+    }
+
     return 0;
 }
 
 std::vector<DataPoint> kmeans(std::vector<DataPoint> data, int k) {
-    std::vector<DataPoint> centroids;
-    std::generate_n(std::back_inserter(centroids), k, randomDatum);
+    auto centroids = randomCentroids(k);
 
-    for (auto &&datum: centroids) {
-        std::cout << datum.instrumentalness << std::endl;
-    }
+    bool changed;
+    int iterations = 0;
 
-    return {};
+    do {
+        changed = assignCentroids(data, centroids);
+        centroids = newCentroids(data, k);
+    } while (changed && iterations++ < max_iterations);
+
+    return data;
 }
 
-void assignCentroids(std::vector<DataPoint> &data, const std::vector<DataPoint> &centroids) {
+bool assignCentroids(std::vector<DataPoint> &data, const std::vector<DataPoint> &centroids) {
+    std::vector<double> distances(centroids.size(), 0.0);
+    bool changed = false;
+
+    for (auto &&datum: data) {
+        std::transform(centroids.begin(), centroids.end(), distances.begin(),
+                       [&](const DataPoint &centroid){ return std::abs(datum - centroid); });
+        size_t min = std::distance(distances.begin(), std::min_element(distances.begin(), distances.end()));
+
+        changed = min != datum.centroid;
+        datum.centroid = min;
+    }
+
+    return changed;
+}
+
+std::vector<DataPoint> newCentroids(const std::vector<DataPoint> &data, int k) {
+    return randomCentroids(k);
 }
