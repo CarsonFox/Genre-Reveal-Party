92e81e3d33165399dca09d1750f7e9a5b65aa6d6
Carson Fox <carson.fox@atomicjolt.com>
2020-12-13 20:43:01 -0500

    Skeleton

--- serial.jl
+++ serial.jl
@@ -19,15 +19,40 @@ datacols = [
     :tempo,
 ]
 
-data = CSV.File(
-    "data/data-small.csv",
-    type = Float64,
-    select = datacols,
-) |> DataFrames.DataFrame
-
-datasize = DataFrames.nrow(data)
-
 rng = Random.MersenneTwister(69)
-random_centroid() = [x for x in data[Random.rand(rng, 1:datasize), datacols]]
 
-print(random_centroid())
+function main()
+    data =
+        CSV.File("data/data-small.csv", type = Float64, select = datacols) |>
+        DataFrames.DataFrame
+
+    kmeans(data, 5)
+end
+
+function kmeans(data, k)
+    centroids = [random_centroid(data) for _ = 1:k]
+    max_iter = 100
+
+    for i = 1:max_iter
+        old_centroids = centroids
+
+        centroids = new_centroids(data, centroids)
+        assign_centroids!(data, centroids)
+
+        if old_centroids == centroids
+            break
+        end
+    end
+end
+
+function new_centroids(data, centroids)
+    centroids
+end
+
+function assign_centroids!(data, centroids) end
+
+function random_centroid(data)
+    [x for x in data[Random.rand(rng, 1:(DataFrames.nrow(data))), datacols]]
+end
+
+main()
